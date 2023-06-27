dd1be8432406d9bd1e2b835f638f21495a815b3b
Roohan Khan <roohank8@gmail.com>
2023-06-28 04:57:22 +0500

    First

--- users/models.py
+++ users/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import Group
 
 # Create your models here.
+
+# s_group = Group.objects.create(name='Students')
+# t_group = Group.objects.create(name='Teachers')
+# j_group = Group.objects.create(name='Judges')
+# m_group = Group.objects.create(name='Mentors')
+# a_group = Group.objects.create(name='Admins')
+
+# student_group = Group.objects.get(name='s_group')
+# teacher_group = Group.objects.get(name='t_group')
+# judge_group = Group.objects.get(name='j_group')
+# mentor_group = Group.objects.get(name='m_group')
+# admin_group = Group.objects.get(name='a_group')
 
--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-
+# from .models import student_group, teacher_group, judge_group, mentor_group, admin_group
 # Create your views here.
 
 # ===============FOR STUDENTS===============
@@ -30,6 +30,7 @@ def student_signup(request):
         else:
             my_user=User.objects.create_user(username, email, password1)
             my_user.save()
+            # my_user.groups.add(student_group)
             return redirect('student_signin')
     return render(request, 'student_signup.html')
 
@@ -42,7 +43,23 @@ def teacher_signin(request):
     return render(request, 'teacher_signin.html')
 
 def teacher_signup(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        email=request.POST.get('email')
+        password1=request.POST.get('password1')
+        password2=request.POST.get('password2')
+        if password1!=password2:
+            return HttpResponse('Passwords do not match')
+        else:
+            my_user=User.objects.create_user(username, email, password1)
+            my_user.save()
+            # my_user.groups.add(teacher_group)
+            return redirect('teacher_signin')
     return render(request, 'teacher_signup.html')
+
+@login_required(login_url='/')
+def student_portal(request):
+    return render(request, 'student_portal.html')
 
 # ===============FOR JUDGES===============
 def judge_signin(request):
