febcf327617af8079cfff10e6547b10c88b1bcea
Roohan Khan <roohank8@gmail.com>
2023-06-29 21:44:01 +0500

    UI betterment

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.db import IntegrityError
 from .models import student_group, teacher_group, judge_group, mentor_group, admin_group
 # Create your views here.
 
@@ -22,17 +23,20 @@ def student_signin(request):
 
 def student_signup(request):
     if request.method=='POST':
-        username=request.POST.get('username')
-        email=request.POST.get('email')
-        password1=request.POST.get('password1')
-        password2=request.POST.get('password2')
-        if password1!=password2:
-            messages.error(request, 'Passwords do not match')
-        else:
-            my_user=User.objects.create_user(username, email, password1)
-            my_user.save()
-            my_user.groups.add(student_group)
+        try:
+            username=request.POST.get('username')
+            email=request.POST.get('email')
+            password1=request.POST.get('password1')
+            password2=request.POST.get('password2')
+            if password1!=password2:
+                messages.error(request, 'Passwords do not match')
+            else:
+                my_user=User.objects.create_user(username, email, password1)
+                my_user.save()
+                my_user.groups.add(student_group)
             return redirect('student_signin')
+        except IntegrityError as e:
+            messages.error(request, 'Username already taken!')
     return render(request, 'student_signup.html')
 
 @login_required(login_url='/')
@@ -59,17 +63,20 @@ def teacher_signin(request):
 
 def teacher_signup(request):
     if request.method=='POST':
-        username=request.POST.get('username')
-        email=request.POST.get('email')
-        password1=request.POST.get('password1')
-        password2=request.POST.get('password2')
-        if password1!=password2:
-            messages.error(request, 'Passwords do not match')
-        else:
-            my_user=User.objects.create_user(username, email, password1)
-            my_user.save()
-            my_user.groups.add(teacher_group)
+        try:
+            username=request.POST.get('username')
+            email=request.POST.get('email')
+            password1=request.POST.get('password1')
+            password2=request.POST.get('password2')
+            if password1!=password2:
+                messages.error(request, 'Passwords do not match')
+            else:
+                my_user=User.objects.create_user(username, email, password1)
+                my_user.save()
+                my_user.groups.add(student_group)
             return redirect('teacher_signin')
+        except IntegrityError as e:
+            messages.error(request, 'Username already taken!')
     return render(request, 'teacher_signup.html')
 
 @login_required(login_url='/')
@@ -95,17 +102,20 @@ def judge_signin(request):
 
 def judge_signup(request):
     if request.method=='POST':
-        username=request.POST.get('username')
-        email=request.POST.get('email')
-        password1=request.POST.get('password1')
-        password2=request.POST.get('password2')
-        if password1!=password2:
-            messages.error(request, 'Passwords do not match')
-        else:
-            my_user=User.objects.create_user(username, email, password1)
-            my_user.save()
-            my_user.groups.add(judge_group)
+        try:
+            username=request.POST.get('username')
+            email=request.POST.get('email')
+            password1=request.POST.get('password1')
+            password2=request.POST.get('password2')
+            if password1!=password2:
+                messages.error(request, 'Passwords do not match')
+            else:
+                my_user=User.objects.create_user(username, email, password1)
+                my_user.save()
+                my_user.groups.add(student_group)
             return redirect('judge_signin')
+        except IntegrityError as e:
+            messages.error(request, 'Username already taken!')
     return render(request, 'judge_signup.html')
 
 # ===============FOR MENTORS===============
@@ -123,17 +133,20 @@ def mentor_signin(request):
 
 def mentor_signup(request):
     if request.method=='POST':
-        username=request.POST.get('username')
-        email=request.POST.get('email')
-        password1=request.POST.get('password1')
-        password2=request.POST.get('password2')
-        if password1!=password2:
-            messages.error(request, 'Passwords do not match')
-        else:
-            my_user=User.objects.create_user(username, email, password1)
-            my_user.save()
-            my_user.groups.add(mentor_group)
+        try:
+            username=request.POST.get('username')
+            email=request.POST.get('email')
+            password1=request.POST.get('password1')
+            password2=request.POST.get('password2')
+            if password1!=password2:
+                messages.error(request, 'Passwords do not match')
+            else:
+                my_user=User.objects.create_user(username, email, password1)
+                my_user.save()
+                my_user.groups.add(student_group)
             return redirect('mentor_signin')
+        except IntegrityError as e:
+            messages.error(request, 'Username already taken!')
     return render(request, 'mentor_signup.html')
 
 # ===============FOR ADMINS===============
@@ -151,17 +164,20 @@ def admin_signin(request):
 
 def admin_signup(request):
     if request.method=='POST':
-        username=request.POST.get('username')
-        email=request.POST.get('email')
-        password1=request.POST.get('password1')
-        password2=request.POST.get('password2')
-        if password1!=password2:
-            messages.error(request, 'Passwords do not match')
-        else:
-            my_user=User.objects.create_user(username, email, password1)
-            my_user.save()
-            my_user.groups.add(admin_group)
+        try:
+            username=request.POST.get('username')
+            email=request.POST.get('email')
+            password1=request.POST.get('password1')
+            password2=request.POST.get('password2')
+            if password1!=password2:
+                messages.error(request, 'Passwords do not match')
+            else:
+                my_user=User.objects.create_user(username, email, password1)
+                my_user.save()
+                my_user.groups.add(student_group)
             return redirect('admin_signin')
+        except IntegrityError as e:
+            messages.error(request, 'Username already taken!')
     return render(request, 'admin_signup.html')
 
 
